b9cd4795b49184436e4fa2fa58063e1febda4bc5
NachoSoto <ignaciosoto90@gmail.com>
2015-02-06 21:15:25 +0000

    Initial commit

---
+++ lib/src/main/java/com/example/MyClass.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class MyClass {
+    private int number;
+
+    public MyClass(final int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
